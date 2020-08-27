c189ab1086511a0c94b7b70930e973e592c1b805
0xTiger <tgh.lizard@googlemail.com>
2020-08-27 17:35:23 +0100

    Add project files.

---
+++ clumpFinderCUDA/clumpFinderCUDA.cu
@@ -0,0 +1,419 @@
+#include <iostream>
+#include <cuda.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <vector>
+#include "PrecomputedRandAdvance.h"
+#include <fstream>
+#include <math.h>
+
+
+#define THREADSPERBLOCK_X 16
+#define THREADSPERBLOCK_Y 16
+#define THREADSPERBLOCK_2 256
+
+// Return the root of a tree
+__device__ unsigned Find(const int* s_buf, unsigned n) {
+
+	unsigned label = s_buf[n];
+
+	while (label - 1 != n) {
+		n = label - 1;
+		label = s_buf[n];
+	}
+
+	return n;
+
+}
+
+// Links together trees containing a and b
+__device__ void Union(int* s_buf, unsigned a, unsigned b) {
+
+	bool done;
+
+	do {
+
+		a = Find(s_buf, a);
+		b = Find(s_buf, b);
+
+		if (a < b) {
+			int old = atomicMin(s_buf + b, a + 1);
+			done = (old == b + 1);
+			b = old - 1;
+		}
+		else if (b < a) {
+			int old = atomicMin(s_buf + a, b + 1);
+			done = (old == a + 1);
+			a = old - 1;
+		}
+		else {
+			done = true;
+		}
+
+	} while (!done);
+
+}
+
+
+// Init phase.
+// Labels start at value 1, to differentiate them from background, that has value 0.
+__global__ void Init(const int* img, int* labels) {
+
+	unsigned row = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned col = blockIdx.x * blockDim.x + threadIdx.x;
+	int globalSize = gridDim.x * blockDim.x;
+
+	unsigned img_index = row * globalSize + col;
+	unsigned labels_index = row * globalSize + col;
+
+
+
+	if (img[img_index]) {
+
+		if (row > 0 && img[img_index - globalSize]) {
+			labels[labels_index] = labels_index - globalSize + 1;
+		}
+
+		else if (col > 0 && img[img_index - 1]) {
+			labels[labels_index] = labels_index;
+		}
+
+		else {
+			labels[labels_index] = labels_index + 1;
+		}
+	}
+
+}
+
+
+// Analysis phase.
+__global__ void Analyze(int* labels) {
+
+	unsigned row = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned col = blockIdx.x * blockDim.x + threadIdx.x;
+	int globalSize = gridDim.x * blockDim.x;
+
+	unsigned labels_index = row * globalSize + col;
+
+	unsigned label = labels[labels_index];
+
+	if (label) {
+
+		unsigned index = labels_index;
+
+		while (label - 1 != index) {
+			index = label - 1;
+			label = labels[index];
+		}
+
+		labels[labels_index] = label;
+	}
+}
+
+__global__ void Reduce(const int* img, int* labels) {
+
+	unsigned row = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned col = blockIdx.x * blockDim.x + threadIdx.x;
+	int globalSize = gridDim.x * blockDim.x;
+
+	unsigned img_index = row * globalSize + col;
+	unsigned labels_index = row * globalSize + col;
+
+
+	if (img[img_index]) {
+
+		if (col > 0 && img[img_index - 1]) {
+			Union(labels, labels_index, labels_index - 1);
+		}
+	}
+}
+
+
+
+__device__ int64_t rawSeedFromChunk(int x, int z)
+{
+	return (((int64_t)x * (int64_t)341873128712 + (int64_t)z * (int64_t)132897987541) ^ (int64_t)0x5DEECE66D) & ((((int64_t)1 << 48) - 1));
+}
+
+
+__device__ int rand5(int64_t raw_seed, int64_t a, int64_t b)
+{
+	return (int)((((raw_seed * a + b) & (((int64_t)1 << 48) - 1)) >> 17) % ((int64_t)5));
+}
+
+
+__device__ int precompChunkIndCalcNormal(int x, int y, int z, int nether)
+{
+	return ((z * 16 + x) * (nether == 1 ? 8 : 4) + ((nether == 1 ? 7 : 3) - y));
+}
+
+
+__device__ int getBedrock(int x, int y, int z, const int64_t* a, const int64_t* b)
+{
+	if (y == 0) return 1;
+	if (y < 0 || y > 4) return 0;
+	int precomp_ind = precompChunkIndCalcNormal(x & 15, y - 1, z & 15, 0);
+	return (rand5(rawSeedFromChunk(x >> 4, z >> 4), a[precomp_ind], b[precomp_ind]) >= y) ? 1 : 0;
+}
+
+
+__global__ void getBedrockTile(const int64_t* a,  const int64_t* b,  const int* offset, int* outData)
+{
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int z = blockIdx.y * blockDim.y + threadIdx.y;
+	int globalSize = gridDim.x * blockDim.x;
+
+	outData[globalSize * x + z] = getBedrock(offset[0] + x, 4, offset[1] + z, a, b);
+}
+
+__global__ void getFrequency(int* labels, int* freq) {
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (labels[x] > 0) {
+		atomicAdd(&(freq[labels[x] - 1]), 1);
+	}
+}
+
+__global__ void reduction(int* inData, int* outData, int* outIdData) {
+	size_t globalId = blockIdx.x * blockDim.x + threadIdx.x;
+	size_t localSize = blockDim.x;
+	size_t localId = threadIdx.x;
+
+	__shared__ int localData[THREADSPERBLOCK_2];
+
+	unsigned bits, var = localSize;
+	for (bits = 0; var != 0; ++bits) var >>= 1;
+
+	localData[localId] = inData[globalId];
+	__syncthreads();
+
+	for (int i = localSize >> 1; i > 0; i >>= 1) {
+		if (localId < i) {
+
+			//localData[localId] = max(localData[localId], localData[localId + i]);
+			if (localData[localId] > localData[localId + i]) {
+				localData[localId + i] = 0; // choose left
+			}
+			else {
+				localData[localId] = localData[localId + i];
+				localData[localId + i] = 1; // choose right
+			}
+		}
+		__syncthreads();
+	}
+
+	if (localId == 0) {
+
+		int bitsum = 0;
+		int nextid;
+		for (int i = 0; i < bits - 1; i++) {
+
+			nextid = (1 << i) + bitsum;
+			bitsum = (localData[nextid] << i) + bitsum;
+		}
+
+		int final_id = localData[nextid] ? nextid : nextid - (localSize >> 1);
+
+		outData[blockIdx.x] = localData[0];
+		outIdData[blockIdx.x] = globalId + final_id;
+	}
+
+
+	//8 is 2^3 so we do 3 hops
+	//0 0 0 0 0 0 0 0
+	//6 | 1 | 0 1 | 0 1 1 1 | -> 1 1 1 -> 8
+	//3 | 0 | 1 0 | 0 1 0 0 | -> 0
+	//9 | 1 | 0 1 | 1 0 1 0 |
+	//7 | 1 | 0 0 | 1 0 1 0 | 0 1 0 0 1 1 1 1 |
+	//0 1 2 3 4 5 6 7
+
+	//select 0th bit from column 1 (1) 1 << 0 + 0
+	//select 1th bit from column 2 (0) 1 << 1 + 1
+	//select 2th bit from column 3 (1) 1 << 2 + 2
+	//select 5th bit from column 4 (1) 1 << 3 + 5
+	/*int bitsum = 0;
+	int nextid;
+	for (int i = 0; i < bits - 2; i++) {
+
+		nextid = (1 << i) + bitsum;
+		bitsum = (bitsum << 1) + localData[nextid];
+	}*/
+}
+
+
+
+
+std::vector<int> spiral(int n) {
+	n++;
+	int k = ceil((sqrt(n) - 1) / 2);
+	int t = 2 * k + 1;
+	int m = t * t;
+	t = t - 1;
+
+	if (n >= m - t) {
+		return { k - (m - n), -k };
+	}
+	else { m = m - t; }
+
+	if (n >= m - t) {
+		return { -k, -k + (m - n) };
+	}
+	else { m = m - t; }
+
+	if (n >= m - t) {
+		return { -k + (m - n), k };
+	}
+	else { return { k, k - (m - n - t) }; }
+}
+
+int main(int argc, char* argv[])
+{
+	int nDevices;
+
+	cudaGetDeviceCount(&nDevices);
+
+	std::cout << nDevices << " devices found" << std::endl;
+	for (int i = 0; i < nDevices; i++) {
+		cudaDeviceProp prop;
+		cudaGetDeviceProperties(&prop, i);
+		std::cout << i << " : " << prop.name;
+		std::cout << " Max threads per block: " << prop.maxThreadsPerBlock << std::endl;
+	}
+	std::cout << "---------------" << std::endl;
+
+	if (argc != 3) {
+		std::cout << "Please enter integer arguments of the form <start> <end> to specify" << std::endl;
+		std::cout << "the range to be searched. Both should be >0 and <67,000,000" << std::endl;
+		return 0;
+	}
+	const int len = 8192;
+	const size_t start = atoi(argv[1]); // 0;
+	const size_t end = atoi(argv[2]); // start + 15;
+
+	std::vector<int> bedrock(len * len, 0);
+	std::vector<int> offset = { 0, 0};
+	
+	std::vector<int> final((len * len) / 256, 0);
+	std::vector<int> finalIds((len * len) / 256, 0);
+	std::vector<int> labels(len * len, 0);
+	std::vector<int> freq(len * len, 0);
+	
+	
+	cudaError_t err;
+
+	int* off_d;
+	int* bedrock_d;
+	int* freq_d;
+	int* final_d;
+	int* finalIds_d;
+	int* labels_d;
+
+	int64_t* a_d, * b_d;
+	err = cudaMalloc(&a_d, sizeof(int64_t) * A_OW_112.size());
+	err = cudaMalloc(&b_d, sizeof(int64_t) * B_OW_112.size());
+	err = cudaMalloc(&off_d, sizeof(int) * offset.size());
+	err = cudaMalloc(&bedrock_d, sizeof(int) * bedrock.size());
+	err = cudaMalloc(&labels_d, sizeof(int) * labels.size());
+	err = cudaMalloc(&final_d, sizeof(int) * final.size());
+	err = cudaMalloc(&finalIds_d, sizeof(int) * finalIds.size());
+	err = cudaMalloc(&freq_d, sizeof(int) * freq.size());
+
+	err = cudaMemcpy(a_d, A_OW_112.data(), sizeof(int64_t) * A_OW_112.size(), cudaMemcpyHostToDevice);
+	err = cudaMemcpy(b_d, B_OW_112.data(), sizeof(int64_t) * B_OW_112.size(), cudaMemcpyHostToDevice);
+	
+	err = cudaMemcpy(bedrock_d, bedrock.data(), sizeof(int) * bedrock.size(), cudaMemcpyHostToDevice);
+	
+	err = cudaMemcpy(final_d, final.data(), sizeof(int) * final.size(), cudaMemcpyHostToDevice);
+	err = cudaMemcpy(finalIds_d, finalIds.data(), sizeof(int) * finalIds.size(), cudaMemcpyHostToDevice);
+
+
+
+	std::tuple<size_t, int, int> best = { 0, 0, 0 };
+
+	//1000 (*1000*1000) takes 7500ms before new shiny kernel
+	//1000 (*1000*1000) takes 2500ms with new shinyish kernel
+	//60 (*4096*4096) takes 2000ms with new shinyish kernel
+	//15 (*8192*8192) takes 2100ms with new shinyish kernel
+	//15 (*8192*8192) takes 2500ms with finished? kernel
+	//15 (*8192*8192) takes 1900ms after neglecting to read freq_buf
+	// ^ at this point after scaling up to 150 iters, we can do about 10^9 blocks per second
+	//15 (*8192*8192) takes 6000ms after first unclean cuda trial
+	//15 (*8192*8192) takes 1000ms after cleaned up cuda memory copys!
+	// ^ at this point after scaling up to 150 iters, we can do about 2.5e9 blocks per second
+	dim3 DimGrid(len / THREADSPERBLOCK_X, len / THREADSPERBLOCK_Y);
+	dim3 DimBlock(THREADSPERBLOCK_X, THREADSPERBLOCK_Y);
+
+	dim3 DimGrid2((len * len) / THREADSPERBLOCK_2);
+	dim3 DimBlock2(THREADSPERBLOCK_2);
+
+	for (int i = start; i < end; i++) {
+		std::cout << i << ' ';
+		offset = { spiral(i)[0] * len , spiral(i)[1] * len };
+		//std::cout << offset[0] << ' ' << offset[1] << std::endl;
+
+		err = cudaMemcpy(off_d, offset.data(), sizeof(int) * offset.size(), cudaMemcpyHostToDevice);
+		getBedrockTile << <DimGrid, DimBlock >> > (a_d, b_d, off_d, bedrock_d);
+
+		//err = cudaMemcpy(bedrock.data(), bedrock_d, sizeof(int) * bedrock.size(), cudaMemcpyDeviceToHost);
+
+		
+		//begin labeling clumps
+		err = cudaMemset(labels_d, 0, sizeof(int) * labels.size());
+
+		Init << <DimGrid, DimBlock >> > (bedrock_d, labels_d);
+		Analyze << <DimGrid, DimBlock >> > (labels_d);
+		Reduce << <DimGrid, DimBlock >> > (bedrock_d, labels_d);
+		Analyze << <DimGrid, DimBlock >> > (labels_d);
+		//finish labeling clumps
+
+		//err = cudaMemcpy(bedrock.data(), bedrock_d, sizeof(int) * bedrock.size(), cudaMemcpyDeviceToHost);
+		//err = cudaMemcpy(labels.data(), labels_d, sizeof(int) * labels.size(), cudaMemcpyDeviceToHost);
+
+		err = cudaMemset(freq_d, 0, sizeof(int) * freq.size());
+
+		getFrequency << <DimGrid2, DimBlock2 >> > (labels_d, freq_d);
+
+		reduction << <DimGrid2, DimBlock2 >> > (freq_d, final_d, finalIds_d);
+		//err = cudaMemcpy(freq.data(), freq_d, sizeof(int) * freq.size(), cudaMemcpyDeviceToHost);
+		err = cudaMemcpy(final.data(), final_d, sizeof(int) * final.size(), cudaMemcpyDeviceToHost);
+		err = cudaMemcpy(finalIds.data(), finalIds_d, sizeof(int) * finalIds.size(), cudaMemcpyDeviceToHost);
+		
+		cudaDeviceSynchronize();
+
+		int record = 0, recordi = 0;
+		for (int i = 0; i < final.size(); i++) {
+			if (final[i] > record) {
+				recordi = i;
+				record = final[i];
+			}
+		}
+		int recordX = finalIds[recordi] / len;
+		int recordZ = finalIds[recordi] % len;
+
+		std::cout << ' ' << record << " @ (" << recordX + offset[0] << ", " << recordZ + offset[1] << ')' << std::endl;
+
+		std::tuple<size_t, int, int> result = { record, recordX + offset[0], recordZ + offset[1] };
+
+		if (std::get<0>(result) > std::get<0>(best)) {
+			best = result;
+		}
+	}
+
+	std::cout << "Best found: " << std::endl;
+	std::cout << std::get<0>(best) << " @ (" << std::get<1>(best) << ", " << std::get<2>(best) << ')' << std::endl;
+
+	cudaFree(a_d);
+	cudaFree(b_d);
+	cudaFree(off_d);
+	cudaFree(bedrock_d);
+	cudaFree(freq_d);
+	cudaFree(final_d);
+	cudaFree(finalIds_d);
+	cudaFree(labels_d);
+
+	std::ofstream outfile;
+	outfile.open("recordFile.txt", std::ios_base::app); // append instead of overwrite
+
+	outfile << "Searched: " << start << '-' << end << " Best found: " << std::get<0>(best) << " @ (" << std::get<1>(best) << ", " << std::get<2>(best) << ')' << std::endl;
+	outfile.close(); 
+}
---
+++ clumpFinderOpenCL/ker.cl
@@ -0,0 +1,211 @@
+// Written by Naoki Shibata shibatch.sf.net@gmail.com 
+// http://ito-lab.naist.jp/~n-sibata/cclarticle/index.xhtml
+
+// This program is in public domain. You can use and modify this code for any purpose without any obligation.
+
+// This is an example implementation of a connected component labeling algorithm proposed in the following paper.
+// Naoki Shibata, Shinya Yamamoto: GPGPU-Assisted Subpixel Tracking Method for Fiducial Markers,
+// Journal of Information Processing, Vol.22(2014), No.1, pp.19-28, 2014-01. DOI:10.2197/ipsjjip.22.19
+/*
+MIT License
+
+Copyright(c) 2019 coolmann24
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
+__kernel void labelxPreprocess_int_int(global int* label, global int* pix, global int* flags, int maxpass, int bgc, int iw, int ih) {
+	const int x = get_global_id(0), y = get_global_id(1);
+	const int p0 = y * iw + x;
+
+	if (y == 0 && x < maxpass + 1) {
+		flags[x] = x == 0 ? 1 : 0;
+	}
+
+	if (x >= iw || y >= ih) return;
+
+	if (pix[p0] == bgc) { label[p0] = -1; return; }
+	if (y > 0 && pix[p0] == pix[p0 - iw]) { label[p0] = p0 - iw; return; }
+	if (x > 0 && pix[p0] == pix[p0 - 1]) { label[p0] = p0 - 1; return; }
+	label[p0] = p0;
+}
+
+__kernel void label4xMain_int_int(global int* label, global int* pix, global int* flags, int pass, int iw, int ih) {
+	const int x = get_global_id(0), y = get_global_id(1);
+	if (x >= iw || y >= ih) return;
+	const int p0 = y * iw + x;
+
+	if (flags[pass - 1] == 0) return;
+
+	int g = label[p0], og = g;
+
+	if (g == -1) return;
+
+	/*for (int yy = -1; yy <= 1; yy++) {
+		for (int xx = -1; xx <= 1; xx++) {
+			if (0 <= x + xx && x + xx < iw && 0 <= y + yy && y + yy < ih) {
+				const int p1 = (y + yy) * iw + x + xx, s = label[p1];
+				if (s != -1 && s < g) g = s;
+			}
+		}
+	}*/
+
+	//considers the 4 cardinal neighbours
+	int xx = 0, yy = 1;
+	if (0 <= x + xx && x + xx < iw && 0 <= y + yy && y + yy < ih) {
+		const int p1 = (y + yy) * iw + x + xx, s = label[p1];
+		if (s != -1 && s < g) g = s;
+	}
+
+	xx = 0, yy = -1;
+	if (0 <= x + xx && x + xx < iw && 0 <= y + yy && y + yy < ih) {
+		const int p1 = (y + yy) * iw + x + xx, s = label[p1];
+		if (s != -1 && s < g) g = s;
+	}
+
+	xx = -1, yy = 0;
+	if (0 <= x + xx && x + xx < iw && 0 <= y + yy && y + yy < ih) {
+		const int p1 = (y + yy) * iw + x + xx, s = label[p1];
+		if (s != -1 && s < g) g = s;
+	}
+
+	xx = 1, yy = 0;
+	if (0 <= x + xx && x + xx < iw && 0 <= y + yy && y + yy < ih) {
+		const int p1 = (y + yy) * iw + x + xx, s = label[p1];
+		if (s != -1 && s < g) g = s;
+	}
+
+	//0,1 1,0 0,-1 -1,0
+
+	for (int j = 0; j < 6; j++) g = label[g];
+
+	if (g != og) {
+		atomic_min(&label[og], g);
+		atomic_min(&label[p0], g);
+		flags[pass] = 1;
+	}
+}
+
+kernel void getFrequency(global int* labels, global int* freq) {
+	int x = get_global_id(0);
+	int z = get_global_id(1);
+
+	int label = labels[get_global_size(0) * x + z];
+	if (label != -1) {
+		atomic_inc(&freq[label]);
+	}
+		
+}
+
+kernel void reduction(global int* inData, local int* localData, global int* outData, global int* outIdData) {
+	size_t globalId = get_global_id(0);
+	size_t localSize = get_local_size(0);
+	size_t localId = get_local_id(0);
+
+
+	unsigned bits, var = localSize;
+	for (bits = 0; var != 0; ++bits) var >>= 1;
+
+	localData[localId] = inData[globalId];
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	for (int i = localSize >> 1; i > 0; i >>= 1) {
+		if (localId < i) {
+
+			//localData[localId] = max(localData[localId], localData[localId + i]);
+			if (localData[localId] > localData[localId + i]) {
+				localData[localId + i] = 0; // choose left
+			}
+			else {
+				localData[localId] = localData[localId + i];
+				localData[localId + i] = 1; // choose right
+			}
+		}
+		barrier(CLK_LOCAL_MEM_FENCE);
+	}
+	
+	
+
+	if (localId == 0) {
+
+		int bitsum = 0; 
+		int nextid;
+		for (int i = 0; i < bits - 1; i++) {
+
+			nextid = (1 << i) + bitsum;
+			bitsum = (localData[nextid] << i) + bitsum;
+		}
+
+		int final_id = localData[nextid] ? nextid : nextid - (localSize >> 1);
+
+		outData[get_group_id(0)] = localData[0];
+		outIdData[get_group_id(0)] = get_global_id(0) + final_id;
+	}
+
+	
+	//8 is 2^3 so we do 3 hops
+	//0 0 0 0 0 0 0 0
+	//6 | 1 | 0 1 | 0 1 1 1 | -> 1 1 1 -> 8
+	//3 | 0 | 1 0 | 0 1 0 0 | -> 0
+	//9 | 1 | 0 1 | 1 0 1 0 |
+	//7 | 1 | 0 0 | 1 0 1 0 | 0 1 0 0 1 1 1 1 |
+	//0 1 2 3 4 5 6 7
+
+	//select 0th bit from column 1 (1) 1 << 0 + 0
+	//select 1th bit from column 2 (0) 1 << 1 + 1
+	//select 2th bit from column 3 (1) 1 << 2 + 2
+	//select 5th bit from column 4 (1) 1 << 3 + 5
+	/*int bitsum = 0;
+	int nextid;
+	for (int i = 0; i < bits - 2; i++) {
+
+		nextid = (1 << i) + bitsum;
+		bitsum = (bitsum << 1) + localData[nextid];
+	}*/
+}
+
+
+
+inline long rawSeedFromChunk(int x, int z)
+{
+	return (((long)x * (long)341873128712 + (long)z * (long)132897987541) ^ (long)0x5DEECE66D) & ((((long)1 << 48) - 1));
+}
+
+
+inline int rand5(long raw_seed, long a, long b)
+{
+	return (int)((((raw_seed * a + b) & (((long)1 << 48) - 1)) >> 17) % ((long)5));
+}
+
+
+inline int precompChunkIndCalcNormal(int x, int y, int z, int nether)
+{
+	return ((z * 16 + x) * (nether == 1 ? 8 : 4) + ((nether == 1 ? 7 : 3) - y));
+}
+
+
+inline int getBedrock(int x, int y, int z, global const long* a, global const long* b)
+{
+	if (y == 0) return 1;
+	if (y < 0 || y > 4) return 0;
+	int precomp_ind = precompChunkIndCalcNormal(x & 15, y - 1, z & 15, 0);
+	return (rand5(rawSeedFromChunk(x >> 4, z >> 4), a[precomp_ind], b[precomp_ind]) >= y)? 1 : 0;
+}
+
+kernel void getBedrockTile(global const long* a, global const long* b, global const int* offset, global int* outData)
+{
+	int x = get_global_id(0);
+	int z = get_global_id(1);
+
+	outData[get_global_size(0) * x + z] = getBedrock(offset[0] + x, 4, offset[1] + z, a, b);
+}
+
+
+
